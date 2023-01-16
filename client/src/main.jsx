ae2cbb93e832146e681285d6f85e2a94063a369a
Adam Bentley <61187053+Adambentleyio@users.noreply.github.com>
2023-01-16 13:40:30 +0000

    named import change

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import {Provider} from 'react-redux'
 import { store } from './app/store'
-import disableReactDevTools from '@fvilers/disable-react-devtools'
+import {disableReactDevTools} from '@fvilers/disable-react-devtools'
 
 if (process.env.NODE_ENV === 'production') {
   disableReactDevTools()
